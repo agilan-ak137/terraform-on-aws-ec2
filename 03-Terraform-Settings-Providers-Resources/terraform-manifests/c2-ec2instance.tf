88e2be09f1a3b4bd28dcffb51ca89cf6df10b674
subha <subha.bharathi16@gmail.com>
2023-08-07 21:35:56 +0530

    dummy-1

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,9 +1,9 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
   ami = "ami-0f34c5ae932e6f0e4"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
-    "Name" = "EC2 Demo"
+    "Name" = "EC2 Demo-2"
   }
 }
